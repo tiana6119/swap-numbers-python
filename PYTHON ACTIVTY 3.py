2a956c6d78a2b375c6b57946ce3a40b9ce886ea7
tiana6119 <tiana6119@gmail.com>
2026-08-25 12:39:04 +0100

    Add files via upload

---
+++ PYTHON ACTIVTY 3.py
@@ -0,0 +1,25 @@
+#Activity 3: Swap Numbers
+
+d = 5
+e = 10
+
+print("Before swapping:")
+print("d =", d)
+print("e =", e)
+
+# Swap using a third variable
+temp = d
+d = d
+e = temp
+
+print("After swapping:")
+print("d =", d)
+print("e =", e)
+
+#Swap without a third variable 
+
+d= 5
+e= 10
+print("After swapping:")
+print("d =", d)
+print("e =", e)
